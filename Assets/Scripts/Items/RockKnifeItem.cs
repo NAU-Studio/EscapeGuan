2eeded739c4f8ac72255cd2071b0070615db4f58
Auleiy <Auleiy@foxmail.com>
2024-07-31 12:30:49 +0800

    修复石刀占用树枝的增益ID的bug

--- Assets/Scripts/Items/RockKnifeItem.cs
+++ Assets/Scripts/Items/RockKnifeItem.cs
@@ -4,9 +4,8 @@ namespace EscapeGuan.Items
 {
     public class RockKnifeItem : MeleeWeapon
     {
-        protected override string ModifierName => "small_stick_item";
+        protected override string ModifierName => "rock_knife_item";
 
-        // 这参数是直接传过去的，所以偷懒不命名了
         public RockKnifeItem(string a, string b, Sprite c) : base(a, b, c, 128, 2.75f, true, 1.15f, true)
         { }
     }
