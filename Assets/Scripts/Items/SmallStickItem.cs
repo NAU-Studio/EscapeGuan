a8794f7a02e2de0da626ea6c5f8cbf95a5d9e1f4
Auleiy <Auleiy@foxmail.com>
2024-07-31 09:19:02 +0800

    （为了方便合成）删除小树枝的耐久度

--- Assets/Scripts/Items/SmallStickItem.cs
+++ Assets/Scripts/Items/SmallStickItem.cs
@@ -1,13 +1,26 @@
+using EscapeGuan.Entities;
 using UnityEngine;
 
 namespace EscapeGuan.Items
 {
-    public class SmallStickItem : MeleeWeapon
+    public class SmallStickItem : Item
     {
-        protected override string ModifierName => "small_stick_item";
+        private readonly Modifier atkModifier = new(1.5f, true), disModifier = new(1.2f, true);
 
         // 这参数是直接传过去的，所以偷懒不命名了
-        public SmallStickItem(string a, string b, Sprite c) : base(a, b, c, 32, 1.5f, true, 1.2f, true)
+        public SmallStickItem(string a, string b, Sprite c) : base(a, b, c)
         { }
+
+        public override void OnHoldUp(ItemStack i)
+        {
+            GameManager.Player.AttackValue.Add("small_stick_item", atkModifier);
+            GameManager.Player.AttackDistance.Add("small_stick_item", disModifier);
+        }
+
+        public override void OnPutDown(ItemStack i)
+        {
+            GameManager.Player.AttackValue.Remove("small_stick_item");
+            GameManager.Player.AttackDistance.Remove("small_stick_item");
+        }
     }
 }
